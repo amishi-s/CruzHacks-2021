3d8ae3b24b13bc115d3e7af009433fd9e92576d9
Amishi Sanghi <amishi.sanghi@gmail.com>
2021-01-17 00:22:02 -0700

    added comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 import spotifyalg
 
-app = Flask(__name__)
+app = Flask(__name__)  #making a new flask app
 
-@app.route('/')
+@app.route('/')   #when you go to the home page (the spotifyalg.py page) it will run the show_spotify func
 def show_spotify():
-    return spotifyalg.main()
+    song_url = spotifyalg.main()  #runs the algorithm and gets the output of that algorithm and passes it to main.html
+    return render_template('main.html', songurl=song_url)  #flask makes main.html as a webpage
 # def hello_world():
 #     return 'Hello, World!'
--- spotifyalg.py
+++ spotifyalg.py
@@ -3,6 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 import random
 
+#do try & except for people who may have copied the guthub code
 try:
     client_id = os.environ["SPOTIPY_CLIENT_ID"]
     client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
@@ -11,22 +12,25 @@ except KeyError as e:
     print(f"Token {e} not found. Please set your environment variable properly. See README. Exiting.")
     exit()
 
+
+scope = 'user-library-read user-read-currently-playing user-read-playback-state'
+
+print('...connecting to Spotify')
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scope))
+
+#make a function so that it can run repeated
 def main():
-    scope = 'user-library-read user-read-currently-playing user-read-playback-state'
-
-    print('...connecting to Spotify')
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scope))
-
     current = sp.current_playback()
 
     try:
-        uri = current["item"]["uri"]
+        uri = current["item"]["uri"]  #get uri of the currently playing song
     except TypeError:
-        return "\n**Not playing anything**\n"
+        return "\n**Not playing anything**\n"  #error message incase ur not playing anything
 
-    features = sp.audio_features([uri])
+    features = sp.audio_features([uri])  #accessing the audio features of the song through spotipy
     features = features[0]
 
+    #different audio features which are on a scale of 0.0 to 1.0
     valence = features["valence"]
     danceability = features["danceability"]
     energy = features['energy']
@@ -35,16 +39,16 @@ def main():
     print(danceability)
     print(energy)
 
-    if valence <= 0.38 and danceability <= 0.6 and energy <= 0.35: 
-        playlist = sp.playlist("spotify:playlist:2cBPRlyEK7ZKl1FvnTtmsP")
+    if valence <= 0.38 and danceability <= 0.6 and energy <= 0.35:   #designated values under which the song is "sad"
+        playlist = sp.playlist("spotify:playlist:2cBPRlyEK7ZKl1FvnTtmsP")  #accessing a specific "Upbeat Positive Tunes" playlist on Spotify
         songs = playlist["tracks"]["items"]
 
-        random_song = random.choice(songs)["track"]["external_urls"]["spotify"]
+        random_song = random.choice(songs)["track"]["external_urls"]["spotify"]  #parsing dictionary and getting a random song
         return random_song
-    else:
-        return "You're already happy :)"
-
-if __name__ == "__main__":
+    
+    return ""
+    
+if __name__ == "__main__":  #the code will only run what ever is in the main func only if we RUN this file specifically 
     main()
 
 
